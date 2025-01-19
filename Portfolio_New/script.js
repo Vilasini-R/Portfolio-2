cddbdd8e6cef1365236a7309a2b31bca00753a20
Vilasini-R <rangampetvilasini26@gmail.com>
2025-01-19 18:41:37 +0530

    Add files via upload

---
+++ Portfolio_New/script.js
@@ -0,0 +1,22 @@
+let roles = ["Full-stack Developer", "Programmer", "Front-end Developer"];
+let currentIndex = 0;
+
+function changeRole() {
+    currentIndex = (currentIndex + 1) % roles.length;
+    document.getElementById("role").textContent = roles[currentIndex];
+}
+
+setInterval(changeRole, 3000);
+
+function toggleDarkMode() {
+    const body = document.body;
+    const toggleButton = document.getElementById("dark-mode-toggle");
+
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        toggleButton.textContent = "Villu";
+    } else {
+        toggleButton.textContent = "Pinky";
+    }
+}
+document.getElementById("dark-mode-toggle").addEventListener("click", toggleDarkMode);
